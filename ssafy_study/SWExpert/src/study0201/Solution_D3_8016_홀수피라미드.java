ed48ed217d4dd9ae50551e1e6f59c9d5b5b2380e
Jaein Lee <skyfin59@naver.com>
2020-02-09 00:57:57 +0900

    first

---
+++ ssafy_study/SWExpert/src/study0201/Solution_D3_8016_홀수피라미드.java
@@ -0,0 +1,33 @@
+package study0201;
+
+import java.util.Scanner;
+
+public class Solution_D3_8016_홀수피라미드 {
+
+	static long T, N;
+	static long first, last;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		T = sc.nextInt();
+		for (int t = 1; t <= T; t++) {
+			N = sc.nextInt();
+			first = 2 * (N-1) * (N-1) +1;
+			last = 2 * N * N-1;
+			//first = 2 * (long) Math.pow(N - 1, 2) + 1;
+			//last = 2 * (long) Math.pow(N, 2) - 1;
+
+			// long Ncnt = N * 2 - 1;
+			// long lidx = 0;
+			// for (int i = 0; i < N; i++) {
+			// lidx += i * 2 + 1;
+			// }
+			// long fidx = lidx - Ncnt + 1;
+			// first = 2 * fidx - 1;
+			// last = 2 * lidx - 1;
+
+			System.out.println("#" + t + " " + first + " " + last);
+		}
+
+	}
+}

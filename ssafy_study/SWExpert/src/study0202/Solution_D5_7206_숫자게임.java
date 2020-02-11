57f26d4b0da6e5862069030e12f18f77db495450
Jaein Lee <skyfin59@naver.com>
2020-02-11 19:21:32 +0900

    0211

--- ssafy_study/SWExpert/src/study0202/Solution_D5_7206_숫자게임.java
+++ ssafy_study/SWExpert/src/study0202/Solution_D5_7206_숫자게임.java
@@ -4,28 +4,36 @@ import java.util.Scanner;
 
 public class Solution_D5_7206_숫자게임 {
 
-	static int T, len;
+	static int T;
 	static String num;
+	static int max;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		T = sc.nextInt();
 		for (int t = 1; t <= T; t++) {
 			num = sc.next();
-			len = num.length();
-			dfs(0, Integer.parseInt(num));
-
+			max = 0;
+			dfs(num, 0);
+			System.out.println("#" + t + " " + max);
 		}
 	}
 
-	public static void dfs(int s, int n) {
-		if (s < 10) {
+	public static void dfs(String n, int cnt) {
+		if (Integer.parseInt(n) < 10) {
+			max = Math.max(max, cnt);
 			return;
 		}
-		for (int i = 1; i < len - 1; i++) {
-			int a = Integer.parseInt(num.substring(0, i));
-			int b = Integer.parseInt(num.substring(i, len));
-			dfs(i + 1, a * b);
+		for (int i = 1; i < n.length(); i++) {
+			for (int j = i + 1; j <= n.length(); j++) {
+				int a = Integer.parseInt(n.substring(0, i));
+				int b = Integer.parseInt(n.substring(i, j));
+				if (j != n.length()) {
+					int c = Integer.parseInt(n.substring(j, n.length()));
+					dfs(String.valueOf(a * b * c), cnt + 1);
+				} else
+					dfs(String.valueOf(a * b), cnt + 1);
+			}
 		}
 	}
 

3786250264351c78fb6300fd7e824ac7c1723747
Jaein Lee <skyfin59@naver.com>
2020-02-19 22:20:41 +0900

    0219

---
+++ ssafy_study/Baekjoon/src/study0203/Main_12851_숨바꼭질2.java
@@ -0,0 +1,47 @@
+package study0203;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main_12851_숨바꼭질2 {
+
+	static int N, K, cnt;
+	static boolean[] visited;
+	static int[] check;
+	static Queue<Integer> q;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		K = sc.nextInt();
+		visited = new boolean[100001];
+		check = new int[3];
+		q = new LinkedList<Integer>();
+		visited[N] = true;
+		q.add(N);
+
+		w:while (!q.isEmpty()) {
+			int qsize = q.size();
+			for (int i = 0; i < qsize; i++) {
+				int n = q.poll();
+				if (n == K) {
+					break w;
+				}
+				check[0] = n - 1;
+				check[1] = n + 1;
+				check[2] = n * 2;
+				for (int d = 0; d < 3; d++) {
+					if (check[d] < 0 || check[d] >= 100001 || visited[check[d]])
+						continue;
+					visited[check[d]] = true;
+					q.add(check[d]);
+				}
+			}
+			cnt++;
+		}
+		System.out.println(cnt);
+
+	}
+
+}
--- ssafy_study/Baekjoon/src/study0203/Main_15683_감시.java
+++ ssafy_study/Baekjoon/src/study0203/Main_15683_감시.java
@@ -1,10 +1,31 @@
 package study0203;
 
+import java.util.Scanner;
+
 public class Main_15683_감시 {
 
+	static int N, M;
+	static int[][] map;
+	static int[] dy = {-1,0,1,0};
+	static int[] dx = {0,1,0,-1};
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		map = new int[N][M];
+		for(int i=0;i<N;i++){
+			for(int j=0;j<M;j++){
+				map[i][j]=sc.nextInt();
+			}
+		}
+		
+		
+		
+		
+	}
+	
+	public static void cctv(int y, int x){
+		
 	}
 
 }

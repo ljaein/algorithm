c3b9383ed43a0d71b9be3b76a64fdfa7581589d9
Jaein Lee <skyfin59@naver.com>
2020-06-25 03:24:26 +0900

    0625

--- ssafy_study/Baekjoon/src/study0201/test.java
+++ ssafy_study/Baekjoon/src/study0201/test.java
@@ -4,14 +4,34 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
 
 public class test {
 
 	public static void main(String[] args) {
-		char c = 'A';
-		System.out.println((char)(c+32));
+		Queue<Point> q = new LinkedList<>();
+		q.add(new Point(1));
+		q.add(new Point(3));
+		q.add(new Point(2));
+		while(!q.isEmpty()){
+			System.out.println(q.poll().a);
+		}
 	}
 
+	static class Point implements Comparable<Point>{
+		int a;
+
+		public Point(int a) {
+			super();
+			this.a = a;
+		}
+
+		@Override
+		public int compareTo(Point o) {
+			return this.a-o.a;
+		}
+		
+	}
 }
